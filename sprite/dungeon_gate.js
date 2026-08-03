b48bd4e018916eca1e99ec0f307412f3220b57ab
Khanhhpk <sonnguyen501tnt@gmail.com>
2026-08-04 00:03:05 +0700

    Chore: Create sprite folder to store custom pixel art notes (penguin & dungeon gate)

---
+++ sprite/dungeon_gate.js
@@ -0,0 +1,20 @@
+// Palette: SPRITES (P)
+// Cổng Hầm Ngục vòm đá, xoáy không gian ma thuật
+export const dungeonGate = [
+  "................",
+  "....MMMMMMMM....",
+  "...MLLLLLLLLM...",
+  "..MLLMCMMCMLLM..",
+  "..MLMvvvvvvMLM..",
+  "..MLvVuuuuVvLM..",
+  "..MLvVuBKuVvLM..",
+  "..MLvVKuuKVvLM..",
+  "..MLvVuuWKVvLM..",
+  "..MLvVKuuuVvLM..",
+  "..MLvVKKuBVvLM..",
+  "..MLvVuKKuVvLM..",
+  "..MLvVuuuuVvLM..",
+  "..MLMvvvvvvMLM..",
+  ".MMLMvvvvvvMLMM.",
+  ".MMMMvvvvvvMMMM."
+];
