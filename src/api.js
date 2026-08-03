9d4d6cfd051127f28c04fb2d5c2163fbe8e40ab8
Khanhhpk <sonnguyen501tnt@gmail.com>
2026-08-03 19:02:18 +0700

    Cleanup: Remove failed Webpack migration files (src folder, package.json, webpack config) to finalize rollback to monolithic architecture

--- src/api.js
+++
@@ -1,9 +0,0 @@
-// API module for SillyTavern integration
-const getContext = () => window.getContext ? window.getContext() : ((typeof window.SillyTavern !== 'undefined' && window.SillyTavern.getContext) ? window.SillyTavern.getContext() : {});
-const ST_context = getContext();
-
-export const extension_settings = window.extension_settings || ST_context.extension_settings || {};
-export const eventSource = window.eventSource || ST_context.eventSource;
-export const event_types = window.event_types || ST_context.event_types;
-export const saveSettingsDebounced = window.saveSettingsDebounced || ST_context.saveSettingsDebounced;
-export const generateRaw = window.generateRaw || ST_context.generateRaw;
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const TEST_MODE = false;
-export const NS = 'star_tavern_farm';
-export const extensionName = 'sillytavern-farm-extension';
-export const RUNTIME_KEY = '__STAR_TAVERN_FARM__';
