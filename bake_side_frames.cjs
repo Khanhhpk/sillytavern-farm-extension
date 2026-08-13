7f5947f1183a7732cc933ee7e6d19dd930e441a2
Khanhhpk <sonnguyen501tnt@gmail.com>
2026-08-13 15:46:17 +0700

    style: balance Sans side walk frames by shifting legs backward

--- bake_side_frames.cjs
+++ bake_side_frames.cjs
@@ -10,12 +10,11 @@ async function main() {
   const l2 = await Jimp.read('sans sprites/04_legs_side/legs_side_02.png');
   
   const comp1 = new Jimp(23, 30);
-  // Using x=5 for l1 makes it align under the body
-  comp1.composite(l1, 5, 23).composite(t1, 5, 13).composite(head, 4, 0);
+  comp1.composite(l1, 7, 23).composite(t1, 5, 13).composite(head, 4, 0);
   await comp1.writeAsync('sans sprites/05_full_body_walk/walk_side_01.png');
   
   const comp2 = new Jimp(23, 30);
-  comp2.composite(l1, 5, 23).composite(l2, 1, 24).composite(t1, 5, 13).composite(head, 4, 0);
+  comp2.composite(l1, 7, 23).composite(l2, 3, 24).composite(t1, 5, 13).composite(head, 4, 0);
   await comp2.writeAsync('sans sprites/05_full_body_walk/walk_side_02.png');
   
   console.log('Done baking side frames');
