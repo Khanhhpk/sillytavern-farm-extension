d7d0fb0c823f4c87c3528cabb3009e6f610e4826
Khanhhpk <sonnguyen501tnt@gmail.com>
2026-08-13 16:03:21 +0700

    chore: clean up temporary test scripts and images

--- inspect_bg.cjs
+++
@@ -1,9 +0,0 @@
-const Jimp = require('jimp');
-
-async function main() {
-  const image = await Jimp.read('sans sprites/01_torso_front/torso_front_01.png');
-  const color = image.getPixelColor(0, 0);
-  const rgba = Jimp.intToRGBA(color);
-  console.log(`Background color:`, rgba);
-}
-main();
